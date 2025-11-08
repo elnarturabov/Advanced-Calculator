8bc328f3b6c78e93c748f816aaaed3a1fd5671c3
Elnar Turabov <turabovelnar@gmail.com>
2025-11-08 18:40:36 +0400

    to add functions about keyboard operators and complete

--- main.js
+++ main.js
@@ -16,22 +16,43 @@ buttons.forEach((btn) => {
     } else if (value === "backspace") {
       input = screeninput.textContent.slice(0, -1);
       screeninput.textContent = input;
+      resizeFont();
     } else if (value === "=") {
-      
-       try {
-    
-    if (screeninput.textContent.trim() === "") return;
+      try {
+        if (screeninput.textContent.trim() === "") return;
+        let expression = screeninput.textContent;
+        // √
+        expression = expression.replace(/√(\d+(\.\d+)?)/g, "Math.sqrt($1)");
+        // factorial
+        expression = expression.replace(/(\d+)!/g, "factorial($1)");
+        // lg
+        expression = expression.replace(/lg(\d+(\.\d+)?)/g, "Math.log10($1)");
+        // ln
+        expression = expression.replace(/ln(\d+(\.\d+)?)/g, "Math.log($1)");
 
-    let result = eval(screeninput.textContent);
-    
-    screenoutput.textContent = result;
-
-  } catch (err) {
-    screenoutput.textContent = "Error";
-  }
+        let result = eval(expression);
+        // Outputa daha əlavə etməsin
+        if (screenoutput.textContent.length >= 15) return; 
+        screenoutput.textContent = result;
+      } catch (err) {
+        screenoutput.textContent = "Error";
+      }
+    } else if (value === "!") {
+      screeninput.textContent += "!";
+      resizeFont();
+    } else if (value === "√") {
+      screeninput.textContent += "√";
+      resizeFont();
+    } else if (value === "lg") {
+      screeninput.textContent += "lg";
+      resizeFont();
+    } else if (value === "ln") {
+      screeninput.textContent += "ln";
+      resizeFont();
     } else {
+      // inputa daha əlavə etməsin
+      if (screeninput.textContent.length >= 26) return; 
       screeninput.textContent += value;
-      operators.style.color = "blue";
       resizeFont();
     }
   });
@@ -47,12 +68,26 @@ function changeDisplayStyle() {
     }
   });
 }
+function factorialCalculate(n) {
+  if (n < 0) {
+    return "Error";
+  }
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+  let result = 1;
+  for (let i = 2; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
 function resizeFont() {
   const length = screeninput.textContent.length;
 
-  if (length > 18) {
+  if (length > 12) {
     screeninput.style.fontSize = "1.8rem";
   } else {
-    screeninput.style.fontSize = "2.4rem"; // default
+    screeninput.style.fontSize = "2.4rem";
   }
 }
